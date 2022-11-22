e4549919d57a51d7d1767db4306f4b9e335a64e5
João Teixeira <j.teixeira.dev@gmail.com>
2022-11-22 14:15:41 +0100

    shopping-list actions

--- src/app/shopping-list/shopping-list.reducer.ts
+++
@@ -1,7 +0,0 @@
-import { Ingredient } from '../shared/ingredient.module';
-
-const initialState = {
-  ingredients: [new Ingredient('Apples', 5), new Ingredient('Tomatoes', 13)],
-};
-
-export function shoppingListReducer(state = initialState, action) {}
